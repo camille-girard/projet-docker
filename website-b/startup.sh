4c5c3645776455450adec9d3baf14f081e71f80f
cgirard <camille.girard1995@gmail.com>
2024-01-14 01:10:23 +0100

    :sparkles: init

---
+++ website-b/startup.sh
@@ -0,0 +1,11 @@
+composer update
+
+composer install --working-dir="/var/www/html"
+
+composer install --working-dir="/var/www/html"
+
+php artisan key:generate
+
+php artisan migrate:fresh --seed
+
+php-fpm
